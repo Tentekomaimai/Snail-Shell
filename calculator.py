59cf20c9e93b66116ddc5a471b11e69c2c968510
Tentekomaimai <chasojuken@yahoo.co.jp>
2017-05-13 15:09:36 +0900

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+import sys
+import time
+try:
+    x = input('Input :')
+    print(float(x))
+except:
+    print("Error! This application has stopped.")
+    sys.exit()
+y = input('keep calcurating → 0 , exit → 1:')
+while y == 0:
+    try:
+        x = input('Input :')
+        print(float(x))
+    except:
+        print("Error! This application has stopped.")
+        sys.exit()
+    y = input('keep calcurating → 0 , exit → 1:')
+if y == 1:
+    print("Good bye")
+    sys.exit()
+else:
+    print("Error! This application has stopped.")
+    sys.exit()
