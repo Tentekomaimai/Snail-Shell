66a320a8a683c219c1f21ef0e36cbff36a39edb7
Tentekomaimai <chasojuken@yahoo.co.jp>
2017-05-13 16:05:19 +0900

    debug

--- calculator.py
+++ calculator.py
@@ -3,18 +3,17 @@ import sys
 import time
 try:
     x = input('Input :')
-    print(int(x))
+    print(float(x))
 except:
-    print("Error! This application has stopped.")
-    sys.exit()
+    print("Error! Try again.")
+    x = input('Input :')
 y = input('keep calcurating → 0 , exit → 1:')
 while y == 0:
     try:
-        x = int('Input :')
-        print(int(x))
+        x = input('Input :')
+        print(float(x))
     except:
-        print("Error! This application has stopped.")
-        sys.exit()
+        print("Error! Try again.")
     y = input('keep calcurating → 0 , exit → 1:')
 if y == 1:
     print("Good bye")
